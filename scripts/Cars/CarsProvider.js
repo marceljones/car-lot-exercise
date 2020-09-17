2d9a569de0a9bbfefe940b5c8211009fde6fa32e
Adam <adam.e.sheaffer@gmail.com>
2020-09-17 11:47:33 -0500

    create instructions and boilerplate

---
+++ scripts/Cars/CarsProvider.js
@@ -0,0 +1,12 @@
+const carsUrl =
+  "https://gist.githubusercontent.com/AdamSheaffer/fe50daf69c713eeb1a83edc2525a4643/raw/3d84acd63a40582c12f47885106071c0ffee6e83/cars.json";
+
+let cars = [];
+
+export const useCars = () => {
+  // TODO
+};
+
+export const getCars = () => {
+  // TODO
+};
---
+++ scripts/Colors/ColorsProvider.js
@@ -0,0 +1,13 @@
+// This is the fetch URL to get an array of available colors
+const availableColorsURL =
+  "https://gist.githubusercontent.com/AdamSheaffer/242f907515474ab081b5a49971bd51f4/raw/fb61fc45a3676a67f7fb5634b987a8f7393b453c/colors.json";
+
+let availableColors = [];
+
+export const useColors = () => {
+  // TODO
+};
+
+export const getAvailableColors = () => {
+  // TODO
+};
