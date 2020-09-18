ca91e9a216b6758a59e23f52d05e1534babde157
Adam Sheaffer <adam.e.sheaffer@gmail.com>
2020-09-18 10:06:24 -0500

    Update ColorsProvider.js

--- scripts/Colors/ColorsProvider.js
+++ scripts/Colors/ColorsProvider.js
@@ -1,6 +1,6 @@
 // This is the fetch URL to get an array of available colors
 const availableColorsURL =
-  "https://gist.githubusercontent.com/AdamSheaffer/fe50daf69c713eeb1a83edc2525a4643/raw/87785ea0ac82007ae1cde7370ffb3b97054b1c41/cars.json";
+  "https://gist.githubusercontent.com/AdamSheaffer/242f907515474ab081b5a49971bd51f4/raw/fb61fc45a3676a67f7fb5634b987a8f7393b453c/colors.json";
 
 let availableColors = [];
 
